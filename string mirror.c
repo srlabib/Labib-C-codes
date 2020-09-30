e1f1f6fe1331cd857836847780c550f1e011305a
Saumya Chitransh <saumyachitransh3001@gmail.com>
2020-09-30 22:18:27 +0530

    cleaner code

--- string mirror.c
+++ string mirror.c
@@ -2,22 +2,22 @@
 int str_lenth(char str[])
 {
     int i;
-    for(i=0; str[i] != '\0'; i++);
+    for (i = 0; str[i] != '\0'; i++)
+        ;
     return i;
 }
 int main()
 {
     char name[200];
     char reslt[200];
-    scanf("%[^\n]%*c",&name);
-    int n = str_lenth(name);
-    int i,j;
-    for(i=0,j=n-1; i<n; i++,j--){
+    scanf("%[^\n]%*c", &name);
+    int n = str_lenth(name), i, j;
+    for (i = 0, j < n; i < n; i++, j--)
+    {
         reslt[j] = name[i];
     }
     reslt[i] = '\0';
-    printf("%s\n",reslt);
+    printf("%s\n", reslt);
 
     return 0;
 }
-
